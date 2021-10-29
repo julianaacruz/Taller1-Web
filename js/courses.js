e42e08f85ba5f8e3d019cf47a9dd393a6da8ebec
julianaacruz <juliana201380cc@gmail.comls -al -/.sshgit config --global user.email juliana201380cc@gmail.com>
2021-10-29 01:24:34 -0500

    courses filters added

--- js/courses.js
+++ js/courses.js
@@ -7,7 +7,7 @@ const products = [
         logo: "https://prod-discovery.edx-cdn.org/organization/logos/44022f13-20df-4666-9111-cede3e5dc5b6-2cc39992c67a.png",
         university: "HarvardX",
         level: "Intermedio",
-        type: "Ciencia de datos",
+        type: "data science",
         instructor: "Rafael Irrizarry"
     },
     {
@@ -18,20 +18,31 @@ const products = [
         logo: "https://prod-discovery.edx-cdn.org/organization/logos/54bc81cb-b736-4505-aa51-dd2b18c61d84-2082c7ba1024.png",
         university: "Berkeley XUniversity",
         level: "Avanzado",
-        type: "Informática",
+        type: "computers",
         instructor: "Steve Johnson"
     },
     {
         id: 3,
-        name: "Inglés para todos",
+        name: "Inglés para todos: Nivel básico",
         price: 120000,
         image: "https://www.britishcouncil.in/sites/default/files/learn_english_british_council.jpg",
         logo: "https://prod-discovery.edx-cdn.org/organization/logos/e0bfb887-eabd-4f4a-8583-7e5aa368b274-cb687d8acb0e.png",
         university: "Universidad del Rosario",
         level: "Básico",
-        type: "Idiomas",
+        type: "languages",
         instructor: "Margareth Harrison"
     },
+    {
+        id: 4,
+        name: "Francés Intermedio",
+        price: 180000,
+        image: "https://esquire-sg.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2020/01/22121302/Steer-deflecting-Conversation-main-image.jpg",
+        logo: "https://prod-discovery.edx-cdn.org/organization/logos/188a8d01-87c1-45e6-b518-3de65906deac-af51133a34ab.png",
+        university: "Sorbonne Université",
+        level: "Intermedio",
+        type: "languages",
+        instructor: "Carla Gagnon"
+    }
 ];
 
 let cart = [];
@@ -47,7 +58,8 @@ const productTemplate = (item) => {
     // Carrito
     const isAdded = cart.some(productCart => productCart.id === item.id);
 
-    console.log(isAdded);
+    console.log(isAdded); //No esta funcionando el some? ****************
+
     let buttonHtml;
     if (isAdded) {
         buttonHtml = `<button class="btn btn--cart" disabled>Curso añadido</button>`
@@ -94,6 +106,49 @@ const productTemplate = (item) => {
     });
 
 };
+
+
+const filterByCategorySelect = document.getElementById("categories");
+const orderBySelect = document.getElementById("orderBy");
+
+const loadProducts = () => {
+    
+    const category = filterByCategorySelect.value || "";
+    const order = orderBySelect.value || "";
+
+    productsSection.innerHTML = "";
+
+    let filteredProductsByCategory; //Cursos Filtrados
+
+    if (category !== "") {
+        filteredProductsByCategory = products.filter((product) => product.type === category);
+    } else {
+        filteredProductsByCategory = products;
+    }
+
+    if (order === "asc") {
+        filteredProductsByCategory = filteredProductsByCategory.sort((a, b) => a.price - b.price);
+    }
+
+    if (order === "desc") {
+        filteredProductsByCategory = filteredProductsByCategory.sort((a, b) => b.price - a.price);
+    }
+    
+    //pintar
+    filteredProductsByCategory.forEach(product => {
+        productTemplate(product);
+    });
+}
+
+
+filterByCategorySelect.addEventListener("change", e => {
+    loadProducts();
+});
+
+orderBySelect.addEventListener("change", e => {
+    loadProducts();
+});
+
 
 //Pintar
 products.forEach(product => {
